9240b4dead533d6499d05252a361df19d6dc7e89
grittonadmas26 <grittonadmas26@gmail.com>
2025-03-06 08:47:56 +0000

    Add 1.py

---
+++ src/1.py
@@ -0,0 +1,11 @@
+import random
+class Person:
+    def __init__(self):
+        self.name = 'Alice'
+        self.age = 30
+        self.country = 'US'
+
+p = Person()
+print(p.name) # prints "Alice"
+print(p.age) # prints 30
+print(p.country) # prints "US"
