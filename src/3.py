3e305b7a07fc9217cd57de1b9922a3b8d5ff772c
grittonadmas26 <grittonadmas26@gmail.com>
2025-03-08 05:32:45 +0000

    Add 3.py

---
+++ src/3.py
@@ -0,0 +1,6 @@
+
+import random
+
+def get_random_code(length):
+    characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    return ''.join(random.choice(characters) for i in range(length))
