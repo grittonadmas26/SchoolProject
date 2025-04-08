d90b3a8baa61d926f042e218624ca5c339e54e5b
grittonadmas26 <grittonadmas26@gmail.com>
2025-04-08 09:30:05 +0000

    Add 26.py

---
+++ src/26.py
@@ -0,0 +1,14 @@
+def calculate_area(length, width):
+    """
+    Calculate the area of a rectangle.
+    
+    Args:
+        length (float): The length of the rectangle.
+        width (float): The width of the rectangle.
+        
+    Returns:
+        float: The area of the rectangle.
+    """
+    return length * width
+
+print(calculate_area(5.0, 3.0))
