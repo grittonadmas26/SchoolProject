d60c8442a0c431a3771774762a21f48fb14952b5
grittonadmas26 <grittonadmas26@gmail.com>
2025-05-24 21:32:27 +0000

    Add 40.py

---
+++ src/40.py
@@ -0,0 +1,11 @@
+import sys
+from time import sleep
+
+def main():
+    # Your code here
+    
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Exiting...")
